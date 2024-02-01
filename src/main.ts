212b0f6b84ca4fe9ba583896a6fc9dd9391f4296
dtmiranda <hortadito2@gmail.com>
2024-02-01 19:35:07 -0100

    allow protection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //whiteList: true will protect app from input/send variable tha is not defined on the DTO 
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   await app.listen(3210);
 }
 bootstrap();
