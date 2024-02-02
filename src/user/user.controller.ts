fdbf27ea79bc2b43cc5eb039186da916048711d3
dtmiranda <hortadito2@gmail.com>
2024-02-02 20:56:55 -0100

    fix path

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { JwtGuard } from '../auth/guard'
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { GetUser } from 'src/auth/decorator/get-user-decorator';
+import { GetUser } from '../auth/decorator';
 
 @UseGuards(JwtGuard)
 @Controller('users')
