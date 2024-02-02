0845100d3a36755933de72b42f987a5f75945e3d
dtmiranda <hortadito2@gmail.com>
2024-02-02 12:51:10 -0100

    ya

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtGuard } from '../auth/guard'
 
 @Controller('users')
 export class UserController {
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('me')
   getMe() {
     return 'user profile'
