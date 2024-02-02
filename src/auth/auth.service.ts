e0d1831d94b44a9e9799a1cd6ece851244ab2ce9
dtmiranda <hortadito2@gmail.com>
2024-02-01 23:26:12 -0100

    implement signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,14 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthServive {
-  constructor(private prisma: PrismaService) { }
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) { }
 
   async singnup(authDto: AuthDto) {
     //generate the password hash
@@ -39,8 +43,32 @@ export class AuthServive {
     }
   }
 
-  signin(authDto: AuthDto) {
-    return "hello im singin"
+  async signin(authDto: AuthDto) {
+    //find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: authDto.email
+      }
+    })
+
+    //if does not exist thrw exception
+    if (!user) {
+      throw new ForbiddenException('Credentials incorret')
+    }
+
+    //compare password
+    const passwordMatches = await argon.verify(
+      user.hash, authDto.password
+    )
+
+    //if password is incorret throw exceptional
+    if (!passwordMatches) {
+      throw new ForbiddenException('Credential incorret')
+    }
+
+    //send back the user 
+    delete user.hash
+    return user
   }
 
 
