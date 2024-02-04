088dab4b23ea37203662619922bc9d51ce6cf680
dtmiranda <hortadito2@gmail.com>
2024-02-04 08:22:23 -0100

    implement edit user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Prisma } from '@prisma/client';
+import { EditUserDto } from './dto';
 
 @Injectable()
 export class UserService {
   constructor(private prismaService: PrismaService) { }
-  async editUser(userId: number) {
+  async editUser(
+    userId: number,
+    dto: EditUserDto,
+  ) {
+    const user = await this.prismaService.user.update({
+      where: {
+        id: userId
+      },
+      data: {
+        ...dto
+      },
+    });
 
+    delete user.hash
+    return user
   }
 }
